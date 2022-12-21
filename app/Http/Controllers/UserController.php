54feaeab9fcaab2efc134a65d6b5d7e6a4ff7c2c
Ayushi <ayushi.sprigstack@outlook.com>
2022-12-21 13:05:51 +0530

    crud operation in user model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,8 +11,8 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     function addUser(Request $request){
-      $user = new User;
-      $user->name = $request->input('UserName');
+   try{   $user = new User;
+      $user->name = $request->input('username');
       $user->email = $request->input('email');
       $user->access_type = $request->input('access');            
       $user->school_id = $request->input('schoolId'); 
@@ -22,19 +22,34 @@ class UserController extends Controller
                 'status' => "success",
                 'msg' => $user
                 )); 
+    }catch (\Throwable $th) {
+            return "something went wrong.".$th;
+        }
     }
-    
     function allUser(){
         $user = User::all();
-        return $user;
+        $array_allUser = array();
+        foreach($user as $userdata){
+         $useraccesstype = $userdata['access_type'];        
+         $Access = Access::where('id',$useraccesstype)->first();
+         $AccessType = $Access['access_type'];        
+         $Schoolid =$userdata['school_id'];
+         $name =$userdata['name'];
+         $email =$userdata['email'];
+         $id =$userdata['id'];        
+          array_push($array_allUser,["Acess"=>$AccessType,"id"=>$id,"school_id"=>$Schoolid,"name"=>$name,"email"=>$email]);
+             } 
+        return $array_allUser;
     }
     
     function updateUser(Request $request){
-        $userID = $request->input('id');
+     try{   $userID = $request->input('id');
         $userName = $request->input('name');
         $userAccessType = $request->input('accessId');       
        $updateUser =User::where('ID', $userID)->update(['name'=>$userName,'access_type'=>$userAccessType]);
        return "success";
+    }catch (\Throwable $th){
+        return "something went wrong.";
     }
     
     function deleteUser(){
@@ -44,4 +59,5 @@ class UserController extends Controller
          $Access = Access::all();
          return $Access;
     }    
+}
 }
